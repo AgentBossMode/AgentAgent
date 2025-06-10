6401c5aa0d7cd390563e8977eef0cb874e13ad28
Kanishk Gupta <51890299+kanishkgupta2000@users.noreply.github.com>
2025-06-11 01:58:23 +0530

    add immersive human-in-loop in composio tool fetching agent

--- experiments/composio_testing/testing_composiotoolset.py
+++ experiments/composio_testing/testing_composiotoolset.py
@@ -5,25 +5,55 @@ from langchain_core.prompts import ChatPromptTemplate
 from composio_langchain import ComposioToolSet, Action
 from composio.client.collections import AppModel
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
-from typing import List
-from pydantic import Field
-from langgraph.types import interrupt
+from typing import List, Literal
+from pydantic import Field, BaseModel
+from langgraph.types import interrupt, Command
 
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
 tool_prompt = """
-You are responsible for getting the right tool given a tool description from Composio
+You are responsible for getting the right tools from Composio given description of the requirements by user.
 
 Follow the following instructions:
-1. Get the list of apps present in Composio using 'get_app' tool.
-2. Check which apps are best matching the user requirements based on their description.
-3. For only the apps retrieved in above step, check the list of actions corresponding to the app using 'get_actions_for_given_app' tool. The tool may return with an empty list, it is fine.
-4. check which of the action schema best suits the requirement, donot suggest actions which are remotely connected to the task at hand.
-                                               
-Respond with the following: 
+1. Respond to the user to understand if they already have any apps/tools that they would like to use for the given requirements. 
+2. Get the list of apps present in Composio using 'get_app' tool.
+3. Check which apps are best matching the user requirements based on their descriptions.
+4. For only the apps retrieved in above step, check the list of actions corresponding to the app using 'get_actions_for_given_app' tool. The tool may return with an empty list, it is fine.
+5. check which of the action schema best suits the requirements, donot suggest actions which are remotely connected to the task at hand.
+6. Post initial analysis respond with the following: 
 
+Corresponding to each input tool description:
 App (there can be multiple entries) - Corresponding to each app, if found provide the exact ActionModel name (donot make human readable), if no actions were found it is okay/
 
+Example:
+
+Input:
+tool_name_1 and tool_description 1
+tool_name_2 and tool_description 2
+
+Output:
+
+*For tool_name_1, here are the following proposed tools:
+
+**App1:
+***action1 --> justification why you think that this action will satisfy tool_description1
+
+**App2:
+***action3 --> justification why you think that this action will satisfy tool_description1
+
+*For tool_name_2, here are the following proposed tools:
+
+**App5:
+***action12 --> justification why you think that this action will satisfy tool_description2
+
+**App6:
+***action2323 --> justification why you think that this action will satisfy tool_description2
+
+
+For both tool_name_1 and tool_name_2, now generate a bunch of questions which would better help understand which tool might help select between the bunch of actions. Most likely the user already knows what tool he needs to integrate.
+
+
+7. If you get human input, responding to the above, now finally provide the final tools to be used corresponding to each user requirement.
 """
 
 composio_toolset = ComposioToolSet()
@@ -66,7 +96,6 @@ def get_actions_for_given_app(app_name: str):
         action_schemas_list += f"""{action_schema.name} - {action_schema.description}\n\n"""
     return action_schemas_list
 
-
 tools =  [get_apps, get_actions_for_given_app]
 
 class ToolBuilderState(MessagesState):
@@ -85,9 +114,16 @@ def agent_node(state: MessagesState):
     response = llm_tools.invoke([SystemMessage(content=tool_prompt)] + state["messages"])
     return {"messages": [response]}
 
-def get_human_review(state: MessagesState):
+class EndOrContinue(BaseModel):
+    should_end_conversation : bool = Field(description="true if the AI response does not indicate that it needs any human input.")
+
+def get_human_review(state: MessagesState) -> Command[Literal["composio_tool_fetch", "select_final_tool"]]:
+    llm_with_struct = llm.with_structured_output(EndOrContinue)
+    should_continue: EndOrContinue = llm_with_struct.invoke([state["messages"][-1]])
+    if should_continue.should_end_conversation:
+        return Command(goto="select_final_tool")
     value = interrupt(state["messages"][-1].content)
-    return {"messages": [HumanMessage(content=value)]} 
+    return Command(goto="composio_tool_fetch", update={"messages": [HumanMessage(content=value)]} ) 
 
 def select_final_tool(state: ToolBuilderState):
     prompt = "You have a list of Apps-Actions and the user has provided which tool to use. Your job is to just respond the final action."
@@ -139,7 +175,6 @@ workflow.add_node("select_final_tool", select_final_tool)
 workflow.add_node("generate_tool_node", generate_tool_node)
 workflow.add_edge(START, "composio_tool_fetch")
 workflow.add_edge("composio_tool_fetch", "human_review")
-workflow.add_edge("human_review", "select_final_tool")
 workflow.add_edge("select_final_tool", "generate_tool_node")
 workflow.add_edge("generate_tool_node", END)
 app = workflow.compile()
