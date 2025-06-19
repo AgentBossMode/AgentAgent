3dbf6deaf133c18eb7328d07e45805681986cad8
Kanishk Gupta <51890299+kanishkgupta2000@users.noreply.github.com>
2025-06-19 09:01:05 +0530

    add dry run logic

--- final_code/nodes/json_generation_node.py
+++ final_code/nodes/json_generation_node.py
@@ -4,6 +4,10 @@ from final_code.states.AgentBuilderState import AgentBuilderState, AgentInstruct
 from final_code.utils.dict_to_reactflow import dict_to_tree_positions
 from final_code.llms.model_factory import get_model
 from final_code.states.NodesAndEdgesSchemas import JSONSchema
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field
+from typing import List
+
 
 llm = get_model()
 
@@ -57,3 +61,40 @@ def json_node(state: AgentBuilderState):
         "justification": json_extracted_output.justification,
         "reactflow_json": reactflow_json
     }
+
+class UseCaseAnalysis(BaseModel):
+    name: str = Field(description="Name of the use case")
+    description: str = Field(description="Description of the use case")
+    dry_run: str = Field(description="Dry run of the use case, which is a string representation of the dry run results")
+
+class DryRunResults(BaseModel):
+    use_cases: List[UseCaseAnalysis] = Field(default_factory=list,description="List of use cases with their names, descriptions, and dry runs.")
+    updated_json_schema: JSONSchema | None = Field(default=None, description="Updated JSON schema if dry run fails")
+    justification: str | None = Field(default=None, description="Justification for updated JSON schema if applicable")
+
+def dry_run_node(state: AgentBuilderState):
+    json_schema: JSONSchema = state["json_schema"]
+    SYS_PROMPT= ChatPromptTemplate.from_template("""
+You are given the json of a workflow graph below.
+{json_schema}
+
+You are also provided with the following user information:
+{agent_instructions}
+You are supposed to write use cases for the graph.
+You will also do dry run of the graph with the use cases.
+The use cases should be in the format of a list of dictionaries.
+Each dictionary should have the following
+keys:
+- name: The name of the use case
+- description: The description of the use case
+- dry_run: The dry run of the use case
+If you find that the dry run fails in any of the cases, you should return the updated json_schema.
+""")
+    llm_with_struct = get_model().with_structured_output(DryRunResults)
+
+    dry_run_analysis:DryRunResults = llm_with_struct.invoke([HumanMessage(content=SYS_PROMPT.format(json_schema=json_schema.model_dump_json(indent=2), agent_instructions=state["agent_instructions"].model_dump_json(indent=2)))])
+    if dry_run_analysis.updated_json_schema is not None:
+        updated_json_schema: JSONSchema = dry_run_analysis.updated_json_schema
+        return {"json_schema": updated_json_schema}
+    else:
+        return        
