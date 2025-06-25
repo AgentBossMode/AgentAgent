db2c9d58225c38ebdc0c4c3068bcaabba1b9533c
abhishekFlick <abhishekc@flickmatch.in>
2025-06-26 01:16:05 +0530

    Creating env_variable fetching node

--- final_code/states/AgentBuilderState.py
+++ final_code/states/AgentBuilderState.py
@@ -26,3 +26,4 @@ class AgentBuilderState(CopilotKitState):
     arch_evaluation_reports: Annotated[list[ArchEvaluationWithUrl], operator.add]
     best_agent_architecture: ArchEvaluationWithUrl = Field(description="the best architecture selected")
     json_code: str = Field(description="Json of the code")
+    env_vairables: list = Field(description="All environment variables required to run the python code")
