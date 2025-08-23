1aacf79cab0eaa538514dd7d05aea115974a5f07
Kanishk Gupta <51890299+kanishkgupta2000@users.noreply.github.com>
2025-08-24 03:03:12 +0530

    removing redundant files from nodes foldr

--- src_folder/app_one_prompt.py
+++ src_folder/app_one_prompt.py
@@ -4,10 +4,7 @@ from final_code.nodes.req_analysis_node import requirement_analysis_node,analyze
 from final_code.nodes.tool_generation_nodev2 import tool_graph
 from final_code.nodes.json_generation_node import json_node
 from final_code.nodes.code_generation_node import code_node, code_analyzer_node
-from final_code.nodes.dfs_analysis_node import dfs_analysis_node
-from final_code.nodes.tool_interrupt import tool_interrupt, add_toolset
-from final_code.nodes.extract_env_var_node import env_var_node
-from final_code.nodes.combine_code_node import combine_code_pipeline_graph
+from final_code.nodes.tool_interrupt import tool_interrupt
 from final_code.nodes.evaluation_pipeline_nodes.mock_tools_writer import mock_tools_writer
 from final_code.nodes.evaluation_pipeline_nodes.pytest_writer import pytest_writer
 from final_code.nodes.evaluation_pipeline_nodes.syntactic_code_reflection import reflection_node
@@ -21,26 +18,20 @@ main_workflow = StateGraph(AgentBuilderState) # Define state type
 main_workflow.add_node("analyze_reqs", analyze_reqs)
 main_workflow.add_node("requirement_analysis_node", requirement_analysis_node)
 main_workflow.add_node("generate_dry_run", generate_dry_run)
-main_workflow.add_node("dry_run_interrupt", dry_run_interrupt) # Renamed node
+main_workflow.add_node("dry_run_interrupt", dry_run_interrupt)
 main_workflow.add_node("json_node", json_node)
-main_workflow.add_node("code_node", code_node)
+main_workflow.add_node("tool_graph", tool_graph)
 main_workflow.add_node("tool_interrupt", tool_interrupt)
-main_workflow.add_node("add_toolset", add_toolset)
-main_workflow.add_node("tool_graph", tool_graph) # Renamed node
+main_workflow.add_node("code_node", code_node)
+main_workflow.add_node("code_analyzer_node", code_analyzer_node)
 main_workflow.add_node("mock_tools_writer", mock_tools_writer)
 main_workflow.add_node("pytest_writer", pytest_writer)
 main_workflow.add_node("reflection", reflection_node)
 main_workflow.add_node("pytest_runner", pytest_runner)
 main_workflow.add_node("evaluation_start", evaluation_start)
 main_workflow.add_node("evaluation_supervisor", evaluation_supervisor)
-main_workflow.add_node("code_analyzer_node", code_analyzer_node)
 
 
-main_workflow.add_node("dfs_analysis_node", dfs_analysis_node)
-main_workflow.add_node("combine_code_pipeline_graph", combine_code_pipeline_graph)
-
-main_workflow.add_node("env_var_node", env_var_node)
-# Define edges for the main workflow
 
 # EDGE SECTION
 
