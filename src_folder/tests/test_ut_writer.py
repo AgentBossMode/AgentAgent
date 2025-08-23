416b0b78dd9c044e5ab275b2f014a53b23d7ef2a
Kanishk Gupta <51890299+kanishkgupta2000@users.noreply.github.com>
2025-08-24 02:51:30 +0530

    fix ut

--- src_folder/tests/test_ut_writer.py
+++ src_folder/tests/test_ut_writer.py
@@ -1,9 +1,11 @@
 import pytest
 from dotenv import load_dotenv
 load_dotenv()
-from src_folder.tests.test_utils.nutrition_agent_files.nutrition_agent_code import nutrition_agent_code
-from src_folder.tests.test_utils.nutrition_agent_files.nutrition_req_analysis import nutrition_dry_runs
-from src_folder.tests.test_utils.nutrition_agent_files.nutrition_mock_tools_code import nutrition_mock_tools_code
+
+from src_folder.tests.test_utils.stock_agent.stock_mock_tools  import stock_mock_tools
+from src_folder.tests.test_utils.stock_agent.stock_main import stock_main
+from src_folder.tests.test_utils.stock_agent.stock_dry_runs import stock_dry_runs
+
 from src_folder.final_code.pydantic_models.UtGen import UtGeneration, TrajectoryUt
 from src_folder.final_code.nodes.evaluation_pipeline_nodes.pytest_writer import generate_ut_llm_call
 from src_folder.final_code.states.ReqAnalysis import DryRuns
@@ -12,7 +14,7 @@ from typing import List
 @pytest.mark.asyncio
 async def test_pytest_writer():
     # Mock state with necessary fields
-    utgenerated: UtGeneration = await generate_ut_llm_call(DryRuns.model_validate_json(nutrition_dry_runs), nutrition_agent_code, nutrition_mock_tools_code)
+    utgenerated: UtGeneration = await generate_ut_llm_call(DryRuns.model_validate_json(stock_dry_runs), stock_main, stock_mock_tools)
     ut_trajectory : List[TrajectoryUt] = utgenerated.trajectory_uts
     assert len(ut_trajectory) > 1, "No trajectory UTs generated"
     
