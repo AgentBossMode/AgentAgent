4cc018dc0e8f8a8548581d8ff0d3697478754f73
Kanishk Gupta <51890299+kanishkgupta2000@users.noreply.github.com>
2025-08-27 16:57:31 +0530

    fix

--- src_folder/final_code/validators_lib/validate_struct_output.py
+++ src_folder/final_code/validators_lib/validate_struct_output.py
@@ -1,6 +1,6 @@
 import ast
-from src_folder.final_code.ast_visitors_lib.StructOutputVisitor import StructOutputVistor
-from src_folder.final_code.ast_visitors_lib.StructOutputClassValidator import StructOutputClassValidator
+from final_code.ast_visitors_lib.StructOutputVisitor import StructOutputVistor
+from final_code.ast_visitors_lib.StructOutputClassValidator import StructOutputClassValidator
 
 def validate_struct_output(code_module: ast.Module):
     structured_output_visitor = StructOutputVistor()
