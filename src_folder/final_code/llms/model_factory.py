cfe2e29b3afb53c72d7b9ee9a4a4001cc9dd4965
Kanishk Gupta <51890299+kanishkgupta2000@users.noreply.github.com>
2025-10-09 21:09:17 +0530

    fix timeouts

--- src_folder/final_code/llms/model_factory.py
+++ src_folder/final_code/llms/model_factory.py
@@ -13,9 +13,9 @@ class ModelName(Enum):
 
 model_dict = {
     ModelName.GPT4oMINI : ChatOpenAI(model="gpt-4o-mini", temperature=0, max_retries=2, timeout = 60),
-    ModelName.GPT41MINI: ChatOpenAI(model="gpt-4.1-mini", temperature=0, max_retries=2, timeout = 60),
+    ModelName.GPT41MINI: ChatOpenAI(model="gpt-4.1-mini", temperature=0, max_retries=2, timeout = 150),
     ModelName.CLAUDE37SONNET: ChatAnthropic(model='claude-3-7-sonnet-20250219', temperature=0, max_retries=2, timeout = 60),
-    ModelName.GEMINI25FLASH: ChatGoogleGenerativeAI(model="gemini-2.5-flash-preview-05-20", temperature=0, thinking_budget=0, max_retries=2, timeout = 60)
+    ModelName.GEMINI25FLASH: ChatGoogleGenerativeAI(model="gemini-2.5-flash-preview-05-20", temperature=0, thinking_budget=0, max_retries=2, timeout = 150)
 }
 
 def get_model(model_name : ModelName= ModelName.GEMINI25FLASH) -> BaseChatModel:
